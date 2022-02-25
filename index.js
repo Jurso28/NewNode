7c47003de3d7f82b28f989190cfed643ea81e38e
Jurso28 <josephurso28@gmail.com>
2022-02-25 16:47:33 -0600

    Update files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+// Define the port.
+const port = process.env.PORT || 8080;
+
+// Define directory where static files will be served to run in the browser.
+app.use(express.static(__dirname + '/public'))
+
+app.get('/hello/', (req, res) => {
+	res.send('Hello World! - JU')
+})
+
+app.get('/mongo/', (req, res) => {
+	res.send('MongoDB! - JU')
+    // Test Mongo DB
+
+
+
+
+
+})
+
+const logMessage = 'Application listening at http://localhost:' + port
+app.listen(port, () => {
+	console.log(logMessage)
+	console.log('...press  Ctrl-C to terminate.\n')
+})
