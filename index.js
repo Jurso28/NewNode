b928dbb73ff06ec416eca5fa8ea1dc2137adca54
Jurso28 <josephurso28@gmail.com>
2022-02-28 15:30:39 -0600

    Verifying hosting still works

--- index.js
+++ index.js
@@ -3,55 +3,11 @@ const app = express()
 
 const {MongoClient} = require('mongodb')
 
-async function main(){
-    const uri="mongodb+srv://TestPerson1:MongoDB@cluster0.khzym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-    const seat = new MongoClient(uri);
-    try{
-        await seat.connect();
-
-        await findRoster(seat, {
-            studentName: true});
-
-        
-
-
-    } catch (e){
-        console.error(e)
-    } finally {
-        await seat.close();
-    }
-    
-
-
-}
-
-main().catch(console.error);
-
 
 
 // Define the port.
 const port = process.env.PORT || 8080;
 
-const server = http.createServer((req,res) => {
-  // normalize url by removing querystring, optional trailing slash, and
-  // making lowercase
-  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
-  switch(path) {
-    case '':
-      serveStaticFile(res, '/public/index.html', 'text/html')
-      break
-    case '/mongo':
-      serveStaticFile(res, '/public/mongo.html', 'text/html')
-      break
-    case '/img/logo.png':
-      serveStaticFile(res, '/public/img/logo.png', 'image/png')
-      break
-    default:
-      serveStaticFile(res, '/public/404.html', 'text/html', 404)
-      break
-  }
-})
 
 // Define directory where static files will be served to run in the browser.
 app.use(express.static(__dirname + '/public'))
@@ -62,29 +18,8 @@ app.get('/hello/', (req, res) => {
 
 app.get('/mongo/', (req, res) => {
 	res.send('MongoDB! - JU')
-    async function updateSeatbyNumber(seat, name, updatedInfo){
-        const result = await seat.db("USS").collection("seatCreation").updateOne({name: name}, {$set: updatedInfo})
+    doMongo()
     
-        console.log(`${result.matchedCount} documents matched the search`);
-        console.log(`${result.modifiedCount} documents were updated`);
-    }
-    
-    async function updateSeatbyNumber30(seat, name, updatedInfo){
-        const result = await seat.db("USS").collection("seatCreation").updateOne({name: name}, {$set: updatedInfo})
-    
-        
-        
-    
-        console.log(`${result.matchedCount} documents matched the search`);
-        console.log(`${result.modifiedCount} documents were updated`);
-    }
-
-
-
-    await updateSeatbyNumber(seat, "Seat 1", {studentName: "Timmy G", userName: "TG"});
-    document.getElementById("Box1").innerHTML = studentName + userName + seat
-    await updateSeatbyNumber30(seat, "Seat 30", {studentName: "Josh W", userName: "JW"});
-    document.getElementById("Box30").innerHTML = studentName + userName + seat
 
 })
 
@@ -93,3 +28,7 @@ app.listen(port, () => {
 	console.log(logMessage)
 	console.log('...press  Ctrl-C to terminate.\n')
 })
+
+function doMongo() {
+    console.log("Do Mongo")
+}
