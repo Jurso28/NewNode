2df986d1470058997e799b0e10f666101c8c45a0
Jurso28 <josephurso28@gmail.com>
2022-02-28 21:39:19 -0600

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 
 //const {MongoClient} = require('mongodb')
@@ -11,25 +11,25 @@ const port = process.env.PORT || 8080;
 
 
 // Define directory where static files will be served to run in the browser.
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.use('/hello/', (req, res) => {
-	res.send('Hello World! - JU')
+	res.send('Hello World! - JU');
 })
 
 app.use('/mongo/', (req, res) => {
-	res.send('MongoDB! - JU')
-    doMongo()
+	res.send('MongoDB! - JU');
+    doMongo();
     
 
 })
 
 const logMessage = 'Application listening at http://localhost:' + port
 app.listen(port, () => {
-	console.log(logMessage)
-	console.log('...press  Ctrl-C to terminate.\n')
+	console.log(logMessage);
+	console.log('...press  Ctrl-C to terminate.\n');
 })
 
 function doMongo() {
-    console.log("Do Mongo")
+    console.log("Do Mongo");
 }
