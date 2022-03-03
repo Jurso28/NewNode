01b663676b9b626229c30913b36f288bb936b516
Jurso28 <josephurso28@gmail.com>
2022-03-02 20:30:45 -0600

    Update

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ const express = require('express');
 const app = express();
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://TestPerson1:MongoDB@cluster0.khzym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const seat = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+seat.connect(err => {
+  const collection = seat.db("USS").collection("seatCreation");
+  // perform actions on the collection object
+
+
+  //seat.close();
+});
+
+
+
 //const {MongoClient} = require('mongodb')
 
+//async function main(){
+    //const uri="mongodb+srv://TestPerson1:MongoDB@cluster0.khzym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+  //  const seat = new MongoClient(uri);
+//	try{
+  //      await seat.connect();
+//		await updateSeatbyNumber(seat, "Seat 1", {studentName: "Timmy G", userName: "TG"});
+
+
+//    } catch (e){
+  //      console.error(e)
+   // } finally {
+    //    await seat.close();
+    //}
+    
+
+
+//}
+//main().catch(console.error);
 
 // Define the port.
 const port = process.env.PORT || 8080;
@@ -13,8 +44,12 @@ const port = process.env.PORT || 8080;
 // Define directory where static files will be served to run in the browser.
 app.use(express.static(__dirname + '/public'));
 
-app.use('/hello/', (req, res) => {
-	res.send('Hello World! - JU');
+app.use(express.static(__dirname + '/public.display'), (req, res) => {
+	updateSeatbyNumber(seat, "Seat 1", {studentName: "Timmy G", userName: "TG"});
+	res.send(document.getElementById("box1").innerText = studentName + "/n"+userName)
+//	document.getElementById("box1").innerText = studentName + "/n"+userName;
+
+
 })
 
 app.use('/mongo/', (req, res) => {
@@ -32,4 +67,14 @@ app.listen(port, () => {
 
 function doMongo() {
     console.log("Do Mongo");
+}
+
+
+function updateSeatbyNumber(seat, name, updatedInfo){
+    const result = seat.db("USS").collection("seatCreation").updateOne({name: name}, {$set: updatedInfo})
+
+    console.log(`${result.matchedCount} documents matched the search`);
+    console.log(`${result.modifiedCount} documents were updated`);
+	//var correctText = document.getElementById("box1").innerText = studentName + "/n"+userName;
+
 }
